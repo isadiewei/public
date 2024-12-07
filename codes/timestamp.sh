a98c664533a714045ee2b62304a0bf99c953f32c
ironsides <isadiewei@pm.me>
2024-12-07 22:46:42 +0000

    + codes/(machine_learning, timestamp)

---
+++ codes/timestamp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# not all these steps are stricly necessary to write
+# placed here for clarity and ease of reading
+
+current_date=$(date -u +"%Y%m%d%H%M%S")
+filename="$current_date.txt"
+
+touch $filename
+
+echo "" >> $filename
+echo "" >> $filename
+echo "-| $current_date" >> $filename
+
+nano $filename
+
